c171a86bc0bdb402d2bee4f0c94c5fa7dd861f16
Rodrigo Oliveira <rodrfons@hotmail.com>
2022-01-28 14:42:13 -0500

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "se17m55l5nq0989f168kj48th",     // CognitoClientID
-  "api_base_url": "https://2gw7z6dbv7.execute-api.us-east-1.amazonaws.com/dev",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "lambda-refarch-webapp-lambda-refarch-webapp.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d10n977vev1zvs.amplifyapp.com"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "2tnt2eopf96bfoomnjbf46c18p",     // CognitoClientID
+  "api_base_url": "https://uns82psx0e.execute-api.us-east-1.amazonaws.com/dev",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-lambda-refarch-webapp-rodrigo.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1znuy543p622o.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
